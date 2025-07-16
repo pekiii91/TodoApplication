8fdaa06892a0366e285a44113cd355dd6fa9432c
pekiii91 <maksimovic.petar991@gmail.com>
2025-07-16 14:15:45 +0200

    Migracija dodavanje kolone Priority

---
+++ TodoApplication/Migrations/20250716084141_AddPriorityToTodo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriorityToTodo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Priority",
+                table: "Todos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Todos");
+        }
+    }
+}
